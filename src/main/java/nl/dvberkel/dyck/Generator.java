5d7aa063128629646f5fc881c641adb1c0e8ba85
dvberkel <daan.v.berkel.1980@gmail.com>
2015-08-09 21:02:03 +0200

    Refactor to generalize for other symbols

--- src/main/java/nl/dvberkel/dyck/Generator.java
+++ src/main/java/nl/dvberkel/dyck/Generator.java
@@ -1,7 +1,29 @@
 package nl.dvberkel.dyck;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Generator {
-    public String next(String word) {
-        return word + "12";
+    private final List<String> symbols;
+
+    public Generator(){
+        this("1", "2");
     }
+
+    public Generator(String left, String right) {
+        this.symbols = Arrays.asList(left, right);
+    }
+
+    public String next(String word) {
+        return initialWordOfLength(word.length() + 2);
+    }
+
+    private String initialWordOfLength(int n) {
+        StringBuilder builder = new StringBuilder();
+        for (int index = 0; index < n; index++) {
+            builder.append(symbols.get(index % 2));
+        }
+        return builder.toString();
+    }
+
 }
