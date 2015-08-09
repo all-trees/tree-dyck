0f2e66e99b77523d964d301324091e1f4ba14ef3
dvberkel <daan.v.berkel.1980@gmail.com>
2015-08-09 20:28:12 +0200

    Initial Generator test

---
+++ src/main/java/nl/dvberkel/dyck/Generator.java
@@ -0,0 +1,7 @@
+package nl.dvberkel.dyck;
+
+public class Generator {
+    public String next(String word) {
+        return "12";
+    }
+}
---
+++ src/test/java/nl/dvberkel/dyck/GeneratorTest.java
@@ -0,0 +1,38 @@
+package nl.dvberkel.dyck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public class GeneratorTest {
+    private String expectedWord;
+    private String word;
+
+    public GeneratorTest(String word, String expectedWord) {
+        this.word = word;
+        this.expectedWord = expectedWord;
+    }
+
+    @Test
+    public void shouldGenerateAllDyckWords(){
+        Generator generator = new Generator();
+
+        String nextWord = generator.next(word);
+
+        assertThat(nextWord, is(expectedWord));
+    }
+
+    @Parameterized.Parameters(name = "next(\"{0}\") is \"{1}\"")
+    public static Collection<Object[]> data(){
+        Collection<Object[]> data = new ArrayList<Object[]>();
+        data.add(new Object[]{ "", "12" });
+        return data;
+    }
+}
